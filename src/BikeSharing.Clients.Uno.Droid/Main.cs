9597363bc795c81554e8f4e2932e9f60b24a1b92
Jérôme Laban <jerome.laban@nventive.com>
2018-07-11 16:52:50 -0400

    Update to latest uno.

--- src/BikeSharing.Clients.Uno.Droid/Main.cs
+++ src/BikeSharing.Clients.Uno.Droid/Main.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 using Android.App;
@@ -10,6 +11,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using BikeSharing.Clients.UWP;
+using Microsoft.Extensions.Logging;
+using Uno.Extensions;
 
 namespace BikeSharing.Clients
 {
@@ -24,6 +27,48 @@ namespace BikeSharing.Clients
 		public MyApplication(IntPtr javaReference, JniHandleOwnership transfer)
 			: base(new App(), javaReference, transfer)
 		{
+			Assembly.Load("Xamarin.Forms.Platform.UAP");
+			Assembly.Load("Xamarin.Forms.Platform");
+			Assembly.Load("Xamarin.Forms.Core");
+			Assembly.Load("Xamarin.Forms.Xaml");
+
+			Xamarin.Forms.Internals.Log.Listeners.Add(new Xamarin.Forms.Internals.DelegateLogListener((c, m) => Console.WriteLine(c + ":" + m)));
+#if DEBUG
+			ConfigureFilters(LogExtensionPoint.AmbientLoggerFactory);
+#endif
+		}
+
+		static void ConfigureFilters(ILoggerFactory factory)
+		{
+			factory
+				.WithFilter(new FilterLoggerSettings
+					{
+						{ "Uno", LogLevel.Warning },
+						{ "Windows", LogLevel.Warning },
+						
+						// Generic Xaml events
+						// { "Windows.UI.Xaml", LogLevel.Debug },
+						// { "Windows.UI.Xaml.Shapes", LogLevel.Debug },
+						// { "Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug },
+						// { "Windows.UI.Xaml.Media", LogLevel.Debug },
+						// { "Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug },
+						// { "Windows.UI.Xaml.UIElement", LogLevel.Debug },
+						// { "Windows.UI.Xaml.Setter", LogLevel.Debug },
+						   
+						// Layouter specific messages
+						// { "Windows.UI.Xaml.Controls", LogLevel.Debug },
+						 { "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
+						// { "Windows.UI.Xaml.FrameworkElement", LogLevel.Debug },
+						   
+						// Binding related messages
+						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
+						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
+						   
+						//  Binder memory references tracking
+						// { "ReferenceHolder", LogLevel.Debug },
+					}
+				)
+				.AddConsole(LogLevel.Debug);
 		}
 	}
 }
